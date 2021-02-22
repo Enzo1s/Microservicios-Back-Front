fc17acc8002a493de69db05eb87916c827c2eeba
Enzo1s <enziitoomartin@gmail.com>
2021-02-22 11:33:53 -0300

    Back-end terminado

---
+++ microservicios-zuul/src/test/java/com/formacionbdi/microservicios/zuul2/MicroserviciosZuul2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.microservicios.zuul2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosZuul2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
